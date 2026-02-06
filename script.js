556f02d11236f98881916813fbeea082f7cc0d9b
MilleFile <173237573+MilleFile@users.noreply.github.com>
2026-02-05 19:19:17 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let rand = Math.random();
     console.log(rand);
@@ -15,7 +18,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let answer = prompt("Enter your choice (rock, paper, scissors): ");
-
+    answer = answer.toLowerCase();
     switch (answer) {
         case("scissors"): 
             return "scissors";
@@ -33,3 +36,40 @@ function getHumanChoice() {
             return null;
     }
 }
+
+
+const humanSelect = getHumanChoice();
+const computerSelect = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("The player wins! Rock beats scissors.");
+        humanScore += 1;
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("The computer wins! Paper beats rock.");
+        computerScore += 1;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("The computer wins! Rock beats scissors.");
+        computerScore += 1;
+    }
+    else if (humanChoice == "scissors" && computerChoice== "paper") {
+        console.log("The player wins! Scissors beats paper.");
+        humanScore += 1;
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("The player wins! Paper beats rock.");
+        humanScore += 1;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("The computer wins! Scissors beats paper.");
+        computerScore += 1;
+    }
+    return null;
+}
+
+playRound(humanSelect, computerSelect);
